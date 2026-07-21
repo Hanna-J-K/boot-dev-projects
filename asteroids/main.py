fc5e69c2e80a98c739aa1dc186274dabbc00ff15
Hanna-J-K <hanna.kraska11@gmail.com>
2026-07-21 16:27:43 +0200

    asteroids: simple refactor, add pygame clock object

--- asteroids/main.py
+++ asteroids/main.py
@@ -3,12 +3,16 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_FILL_COLOR
 from logger import log_state 
 
 def main():
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    game_clock = pygame.time.Clock()
+    delta_time = 0.0
 
     while True:
         log_state()
@@ -19,6 +23,8 @@ def main():
 
         screen.fill(SCREEN_FILL_COLOR)
         pygame.display.flip()
+
+        delta_time = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
