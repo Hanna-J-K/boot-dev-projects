cb145f435dc4c2c16e1895e5f56200f2576b04bf
Hanna-J-K <hanna.kraska11@gmail.com>
2026-07-21 19:28:44 +0200

    asteroids: add 60fps limit

--- asteroids/main.py
+++ asteroids/main.py
@@ -24,7 +24,7 @@ def main():
         screen.fill(SCREEN_FILL_COLOR)
         pygame.display.flip()
 
-        delta_time = clock.tick(60) / 1000
+        delta_time = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
