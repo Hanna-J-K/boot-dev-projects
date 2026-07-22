36a3721d896d76b5fd2681bc25e9355a8a60a2b7
Hanna-J-K <hanna.kraska11@gmail.com>
2026-07-22 15:10:36 +0200

    asteroids: add rudimentary shooting

--- asteroids/player.py
+++ asteroids/player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x: float, y: float) -> None:
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown = 0
 
     def triangle(self) -> list[pygame.Vector2]:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -30,11 +31,16 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self) -> None:
+        if self.shot_cooldown > 0:
+            return
+        else:
+            self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)
         shot.velocity *= PLAYER_SHOOT_SPEED
 
     def update(self, delta_time: float) -> None:
+        self.shot_cooldown -= delta_time
         keys = pygame.key.get_pressed()
 
         # move
