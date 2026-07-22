5378f54e9e2d3426e0d8973e8eccdf02e7b5cf2f
Hanna-J-K <hanna.kraska11@gmail.com>
2026-07-22 11:52:18 +0200

    asteroids: add drawing player ship at the center

--- asteroids/constants.py
+++ asteroids/constants.py
@@ -1,3 +1,9 @@
 SCREEN_WIDTH: int = 1280
 SCREEN_HEIGHT: int  = 720
+PLAYER_RADIUS: int = 20
+LINE_WIDTH: int = 2
+
+
+# colors
 SCREEN_FILL_COLOR: str = "black"
+PLAYER_COLOR: str = "white"
--- asteroids/main.py
+++ asteroids/main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_FILL_COLOR
 from logger import log_state 
+from player import Player
 
 def main():
 
@@ -12,8 +13,13 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     game_clock = pygame.time.Clock()
-    delta_time = 0.0
+    delta_time: float = 0.0
+    
+    center_position_x: float = SCREEN_WIDTH / 2
+    center_position_y: float = SCREEN_HEIGHT / 2
 
+    player = Player(center_position_x, center_position_y)
+    
     while True:
         log_state()
         
@@ -22,9 +28,12 @@ def main():
                 return
 
         screen.fill(SCREEN_FILL_COLOR)
+        
+        player.draw(screen)
+
         pygame.display.flip()
-
         delta_time = game_clock.tick(60) / 1000
-
+        
+        
 if __name__ == "__main__":
     main()
